75c70813f4dd3c6e20eadfa2888919068e3a3747
Gabriel Morais <77515545+GabrielACM11@users.noreply.github.com>
2022-05-13 22:14:59 -0300

    7

--- exer7-Arrays/script.js
+++ exer7-Arrays/script.js
@@ -1,10 +1,20 @@
-// exercicio 3 - pode beber?
-idade = parseInt(prompt('qual a sua idade?'))
+//Vamos testar alguns arrays
 
-if(idade >= 18){
-    alert('pode beber! Mostre sua identidade')
-}
-else{
-    alert('não pode! volte futuramente.')
+var alunos = ['gabriel', 'igor', 'maria', 'julia'];
+//tambem pode ser criado como var alunos = new Array('gabriel', 'igor', 'maria', 'julia');
+//array começa no 0
+
+//o .length puxa o numero de dados(neste caso 4 alunos) dessa array
+//entao se eu adicionar mais alunos no array, o codigo vai acompanhar
+for(var i = 0; i < alunos.length; i++){
+    console.log(alunos[i])
 }
 
+//outras formas
+/*for (var i in alunos){
+    console.log(alunos[i])
+}
+
+for(var aluno of alunos){
+    console.log(aluno)
+}*/
